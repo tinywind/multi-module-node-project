4c877f735d62a1bca383c90b465cd0561d08806c
tinywind <tinywind@saerosoft.com>
2024-10-14 12:45:16 +0700

    init

---
+++ projectA/src/index.ts
@@ -0,0 +1,4 @@
+import User from 'model/dist/User';
+
+const user = new User('Alice', 30);
+console.log(user.getInfo());
---
+++ projectB/src/index.ts
@@ -0,0 +1,4 @@
+import User from 'model/dist/User';
+
+const user = new User('Bob', 25);
+console.log(user.getInfo());
